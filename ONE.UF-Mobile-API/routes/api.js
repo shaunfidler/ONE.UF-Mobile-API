7408d295e05425c860373aaa97b177ca43c7f180
Shaun Fidler <shaunfidler@ufl.edu>
2022-02-22 16:38:10 -0500

    Add getgrades endpoint

--- ONE.UF-Mobile-API/routes/api.js
+++ ONE.UF-Mobile-API/routes/api.js
@@ -27,4 +27,20 @@ router.get('/uf/user', function(req, res, next) {
   })
 });
 
+/* GET grades. */
+router.get('/transcript/getgrades', function(req, res, next) {
+  shibsession = req.cookies["_shibsession_68747470733a2f2f73712e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f68747470733a2f2f73702e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f"]
+  userModel.find({ "_shibsession_68747470733a2f2f73712e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f68747470733a2f2f73702e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f": shibsession })
+  .then((users) => {
+    res.setHeader('Content-Type', 'application/json');
+    if(users.length != 0) 
+    {
+      res.send(users[0].res_getgrades)
+    }else
+    {
+      res.send({"error": "User not found."})
+    }    
+  })
+});
+
 module.exports = router;
