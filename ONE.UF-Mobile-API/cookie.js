951fe2ebcde47a82c635954acadce2ccd8b6c4de
Shaun Fidler <shaunfidler@ufl.edu>
2022-02-22 02:10:40 -0500

    Introduce MongoDB to Mock API

---
+++ ONE.UF-Mobile-API/cookie.js
@@ -0,0 +1,24 @@
+const shibsession = "_5683892e3f11ea5ea384c15b4e";
+
+module.exports = {
+  shaun : 
+  {
+    "shibsession": shibsession + "_SHAUN"
+  },
+  charles : 
+  {
+    "shibsession": shibsession + "_CHARLES"
+  },
+  brody : 
+  {
+    "shibsession": shibsession + "_BRODY"
+  },
+  ben : 
+  {
+    "shibsession": shibsession + "_BEN"
+  },
+  greg : 
+  {
+    "shibsession": shibsession + "_GREG"
+  }
+};
--- ONE.UF-Mobile-API/routes/api.js
+++ ONE.UF-Mobile-API/routes/api.js
@@ -1,23 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var userModel = require('../models/userModel.js');
+
+var getUser = async() => {
+  let found = await userModel.find();//findOne( { "_shibsession_68747470733a2f2f73712e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f68747470733a2f2f73702e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f": "_5683892e3f11ea5ea384c15b4e_SHAUN" } );
+  console.log(found);
+  return found;
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('ONE.UF Mocked API');
+  user = 
+  {
+    "_shibsession_68747470733a2f2f73712e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f68747470733a2f2f73702e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f": "TEST_USER",
+    "res_users": "TEST_RES"
+  }
+  userModel.create(user);
 });
 
 /* GET user. */
 router.get('/uf/user', function(req, res, next) {
-  data = 
-  {
-    "name": "Shaun Middlename Fidler",
-    "legalName": "Fidler,Shaun Middlename",
-    "ufid": "12345678",
-    "apps": "[]",
-    "email": "username@ufl.edu",
-    "switchUser": "false"
-  }
-  res.send(data);
+  user_id = "" //req.cookies["_shibsession_68747470733a2f2f73712e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f68747470733a2f2f73702e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f"]
+  userModel.find()
+  .then((user) => {
+    console.log(user)
+    res.send(user)
+  })
 });
 
 module.exports = router;
