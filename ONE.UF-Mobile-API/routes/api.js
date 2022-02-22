5f71012b940ba837581edfd946b9411f661dedef
Shaun Fidler <shaunfidler@ufl.edu>
2022-02-22 16:19:02 -0500

    Removed grades endpoint that hsould not be there

--- ONE.UF-Mobile-API/routes/api.js
+++ ONE.UF-Mobile-API/routes/api.js
@@ -22,22 +22,6 @@ router.get('/uf/user', function(req, res, next) {
       res.send(users[0].res_users)
     }else
     {
-      res.send({"error": "User not found"})
-    }    
-  })
-});
-
-/* GET grades. */
-router.get('/uf/user', function(req, res, next) {
-  shibsession = req.cookies["_shibsession_68747470733a2f2f73712e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f68747470733a2f2f73702e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f"]
-  userModel.find({ "_shibsession_68747470733a2f2f73712e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f68747470733a2f2f73702e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f": shibsession })
-  .then((users) => {
-    res.setHeader('Content-Type', 'application/json');
-    if(users.length != 0) 
-    {
-      res.send(users[0].res_user)
-    }else
-    {
       res.send({"error": "User not found."})
     }    
   })
