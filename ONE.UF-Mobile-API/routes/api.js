652b9b5e7006fd5daaf3b0fa1ce87fcacb3f8b7b
Shaun Fidler <shaunfidler@ufl.edu>
2022-04-09 16:27:39 -0400

    Remove DB dump endpoint as we near PROD

--- ONE.UF-Mobile-API/routes/api.js
+++ ONE.UF-Mobile-API/routes/api.js
@@ -2,15 +2,6 @@ var express = require('express');
 var router = express.Router();
 var userModel = require('../models/userModel.js');
 
-/* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.setHeader('Content-Type', 'application/json');
-  userModel.find()
-  .then((users) => {
-    res.send(users);
-  });
-});
-
 /* GET user. */
 router.get('/uf/user', function(req, res, next) {
   shibsession = req.cookies["_shibsession_68747470733a2f2f73712e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f68747470733a2f2f73702e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f"]
